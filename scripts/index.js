942af903553ff9f4e4290e71558435d56ec3b5df
kaiqueramos <kaiqueramospessoal@gmail.com>
2020-10-26 20:14:25 -0300

    Slider das imagens - posts e imagens

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,18 @@
 function show(item){
     let imgClass = document.getElementById("image");
     let tweetClass = document.getElementById("tweet");
+    let imglist = document.getElementById("imagelist");
+    let tweetlist = document.getElementById("tweetlist");
     if (item === 'image'){
         imgClass.className = "postimage-responsive-tweets postimage-responsive-active"
         tweetClass.className = "postimage-responsive-tweets"
+        imglist.className = "postimage-imagelist"
+        tweetlist.className = "postimage-postlist hide-content"
     }
     if (item === 'tweet') {
         tweetClass.className = "postimage-responsive-tweets postimage-responsive-active"
         imgClass.className = "postimage-responsive-tweets"
+        imglist.className = "postimage-imagelist hide-content"
+        tweetlist.className = "postimage-postlist"
     }
 }
