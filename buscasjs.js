895cc077c0b92c14eaf8a759417b9cf948a7d568
melissa <melissalongatti@gmail.com>
2020-10-26 15:50:48 -0300

    adicionando botoes de avancar e voltar

---
+++ buscasjs.js
@@ -0,0 +1,25 @@
+<button onclick="myFunction()">Click me</button>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const { domain } = require("process")
+
+///for list
+
+//pegar os dados da dom, fazer rolamento da pageXOffset, triplicar buscas pra aparecer mais # assim que rolar a pagina
+
+//nao vai usar dados reais por enquanto
